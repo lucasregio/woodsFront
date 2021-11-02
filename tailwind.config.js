a9623f3f429704583bd4a25dd7f4c47a00cb075a
Lucas Regio <souzaregiolucas@gmail.com>
2021-11-02 20:35:04 -0300

    configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'woods': "url('/src/resources/images/woodsbg.jpg')",
+        'closet': "url('/src/resources/images/unnamed.jpg')",
+        'florest': "url('/src/resources/images/74728.jpg')", 
+       }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      animation: ['motion-safe'],
+    },
   },
   plugins: [],
+  
 }
